28dca77aea76bad884a8cabea8007c54d29b3092
Prof Anderson Andrade Silva <andersonandrades@prof.educacao.sp.gov.br>
2025-07-07 14:57:38 -0300

    App streamlit

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
 import streamlit as st
+from transform import *
 
+st.title('Gerador de plano de aula')
+st.caption('Preencha o formulário e submeta para baixar o plano!')
+
+data = 'Escopo.xlsx'
+disci = disciplinas(data)
+disciplina = st.selectbox('Disciplina', disci)
+ano = st.selectbox('Ano/série:', ['1ª', '2ª', '3ª'])
+bimestre = st.selectbox('Bimestre', ['1º', '2º', '3º', '4º'])
+aulas = []
+
+if disciplina:
+    df = data_frame(data, disciplina)
+    aula = qtde_aulas(df, ano, bimestre)
+    for a in range(1, aula + 1):
+        aulas.append(a)
+    opc = st.multiselect('Selecione',aulas)
+    for option in opc:
+        st.dataframe(filtro(df, ano, bimestre, option))
+            
 
 
 
--- transform.py
+++ transform.py
@@ -1,5 +1,13 @@
 import pandas as pd
 
+
+def disciplinas(df):
+    xls = pd.ExcelFile(df)
+    abas = xls.sheet_names
+    del abas[0]
+    return abas
+
+
 def data_frame(file_path, sheet_name):
     # Carregar os dados do arquivo Excel
     df = pd.read_excel(file_path, sheet_name)
@@ -16,9 +24,17 @@ def data_frame(file_path, sheet_name):
     return df
 
 
+def qtde_aulas(df, ano_serie, bimestre):
+    df_contado = df[(df['ANO/SÉRIE'] == ano_serie) & (df['BIMESTRE'] == bimestre)].shape[0]
+    return df_contado
+
+
+
 def filtro(df, ano_serie, bimestre, aula):
     df_filtrado = df[(df['ANO/SÉRIE'] == ano_serie) & (df['BIMESTRE'] == bimestre) & (df['AULA'] == aula)]
     return df_filtrado
+
+
 
 
 # if __name__ == '__main__':
@@ -26,7 +42,11 @@ def filtro(df, ano_serie, bimestre, aula):
 #     df = data_frame(data, 'Matemática')
 #     ano = input("O Ano-série: ")
 #     bimestre = input('O bimestre: ')
+#     df_filtrado = df[(df['ANO/SÉRIE'] == ano) & (df['BIMESTRE'] == bimestre)].shape[0]
+#     print(df_filtrado)
 #     aula = int(input('A aula: '))
+#     disciplina = disciplinas(df=data)
+#     print(disciplina)
 #     df_escolhido = filtro(df=df, bimestre=bimestre, ano_serie=ano, aula = aula)
 #     print(df_escolhido)
 
